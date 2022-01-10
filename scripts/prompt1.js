14cebb6c4cd504dae30d9664a3c1b9847e533cd5
Mohammed Alam <mohammedalam52@gmail.com>
2022-01-10 12:01:19 -0500

    updated html and js

--- scripts/prompt1.js
+++ scripts/prompt1.js
@@ -1,3 +1,27 @@
-document.addEventListener("DOMContentLoaded", function () {
-  alert("hello");
-});
+/*document.addEventListener("DOMContentLoaded", Prompt1);
+
+
+function Prompt1() {
+  const button1 = document.getElementById("btn-1");
+  const button2 = document.getElementById("btn-2");
+
+  function imRight(){
+    document.querySelector("#p").innerHTML = "Im Right"
+  }
+
+  button1.addEventListener('click', imRight())
+  
+  button2.addEventListener('click', function() {
+
+  })
+}
+*/
+function buttonOne(){
+
+  document.getElementById("textButton").textContent = "I'm right!";
+}
+
+function buttonTwo(){
+
+  document.getElementById("textButton").textContent = "No I'm right!";
+}
