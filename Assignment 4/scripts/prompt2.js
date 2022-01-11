819244447407e71e44e3582e7141a702501c3fc0
Gilman Huang <gilman.huang213@gmail.com>
2022-01-11 01:58:51 -0500

    Delete Assignment 4 directory

--- Assignment 4/scripts/prompt2.js
+++
@@ -1,5 +0,0 @@
-let dontHover = document.getElementById("hover")
-
-noHover.addEventListener("mouseover", function (event) {
-    alert("Hey, I told you not to hover over me!")
-})
--- Assignment 4/scripts/prompt3.js
+++
@@ -1,6 +0,0 @@
-let form = document.querySelector('form');
-form.addEventListener('submit', (check) => {
-	console.log(check);
-	if (document.getElementById('password').value != 123456789) alert(`Incorrect password`);
-	else document.querySelector('h1').innerText = 'Correct!';
-});
