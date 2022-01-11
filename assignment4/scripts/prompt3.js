31beb297de546d8118e8e5f2148f816375414035
Gilman Huang <67421795+Gily-H@users.noreply.github.com>
2022-01-11 09:50:07 -0500

    cleanup and formatting files

--- assignment4/scripts/prompt3.js
+++ assignment4/scripts/prompt3.js
@@ -1,9 +1,9 @@
-function checkPassword(){
-	event.preventDefault();
-	let password = document.getElementById('password').value;
-	if(password != "12345678"){
-		alert("Incorrect password");
-	}else{
-		document.getElementById("status").textContent = "Correct!";
-	}
+function checkPassword() {
+  event.preventDefault();
+  let password = document.getElementById("password").value;
+  if (password != "12345678") {
+    alert("Incorrect password");
+  } else {
+    document.getElementById("status").textContent = "Successful!";
+  }
 }
