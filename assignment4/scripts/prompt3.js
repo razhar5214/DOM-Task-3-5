0951ba13b09bdfc1d5e88da2c6497e5895809e1f
Gilman Huang <67421795+Gily-H@users.noreply.github.com>
2022-01-11 09:12:31 -0500

    assignment4-gh-pages

--- assignment4/scripts/prompt3.js
+++
@@ -1,9 +0,0 @@
-function checkPassword(){
-	event.preventDefault();
-	let password = document.getElementById('password').value;
-	if(password != "12345678"){
-		alert("Incorrect password");
-	}else{
-		document.getElementById("status").textContent = "Correct!";
-	}
-}
